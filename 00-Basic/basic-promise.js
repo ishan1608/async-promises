6c6e28f38d345213286e4b6425f2b67b697e35cb
Ishan <ishan1608@gmail.com>
2018-06-01 00:16:10 +0530

    Added a basic promise example

---
+++ 00-Basic/basic-promise.js
@@ -0,0 +1,40 @@
+var fetchFromApi = function(post_number) {
+    var postPromise = new Promise(function(resolve, reject) {
+        // Do the usual XHR stuff
+        var req = new XMLHttpRequest();
+        req.open('get', 'https://jsonplaceholder.typicode.com/posts/' + post_number);
+
+        req.onload = function() {
+            // 'load' triggers for 404s etc
+            // so check the status
+            if (req.status === 200) {
+                // Resolve the promise with the response text
+                resolve(req.response);
+            } else {
+                // Otherwise reject with the status text
+                reject(Error(req.statusText));
+            }
+        };
+
+        // Handle network errors
+        req.onerror = function() {
+            reject(Error("Network Error"));
+        };
+
+        // Make the request
+        req.send();
+    });
+    return postPromise;
+};
+
+fetchFromApi(10).then(function(result) {
+    console.log('====================================');
+    console.log('Response for 10');
+    console.log(result);
+    console.log('====================================')
+}, function(err) {
+    console.log('====================================');
+    console.error('Error for 10');
+    console.error(err);
+    console.log('====================================')
+});
